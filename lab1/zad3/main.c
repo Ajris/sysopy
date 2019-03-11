36bf267d905ad603b37fc1c6391b38922ee1d1f0
Ajris <hubiflower@gmail.com>
2019-03-11 01:28:16 +0100

    fixed zad 3

--- lab1/zad3/main.c
+++ lab1/zad3/main.c
@@ -20,6 +20,7 @@ void saveRaport(char **raport, int size);
  VOID search_directory CHAR* directory CHAR* fileToSearch CHAR* fileToSave
  VOID remove_block INT indexOfBlock
  INT save_block CHAR* fileToSave
+ VOID save_and_remove CHAR* fileToSave INT numOfOperations
  */
 
 int main(int argc, char **argv) {
@@ -35,15 +36,16 @@ int main(int argc, char **argv) {
     printf("\n");
 
     struct Result *result;
-    raport[0] = "REAL - SYS - USER";
+    raport[0] = "\nREAL - SYS - USER\n";
     int currentIndex = 1;
     for (int i = 1; i < argc; i++) {
         int operationNum = 0;
+
+        savedBegin.real = times(begin);
+        savedBegin.sys = begin->tms_stime;
+        savedBegin.user = begin->tms_utime;
         if (strcmp(argv[i], "create_table") == 0) {
             operationNum = 1;
-            savedBegin.real = times(begin);
-            savedBegin.sys = begin->tms_stime;
-            savedBegin.user = begin->tms_utime;
             if (i + 1 >= argc) {
                 printf("Error in input, not enough parameters for creating table");
                 return 0;
@@ -51,15 +53,8 @@ int main(int argc, char **argv) {
             char *tmp;
             result = createTable((size_t) strtol(argv[i + 1], &tmp, 0));
             i = i + 1;
-            savedEnd.real = times(end);
-            savedEnd.sys = end->tms_stime;
-            savedEnd.user = end->tms_utime;
         } else if (strcmp(argv[i], "search_directory") == 0) {
             operationNum = 2;
-            savedBegin.real = times(begin);
-            savedBegin.sys = begin->tms_stime;
-            savedBegin.user = begin->tms_utime;
-
             if (i + 3 >= argc) {
                 printf("Error in input, not enough parameters for search");
                 return 0;
@@ -69,14 +64,8 @@ int main(int argc, char **argv) {
             char *fileToSave = argv[i + 3];
             searchFile(directory, fileToSearch, fileToSave);
             i = i + 3;
-            savedEnd.real = times(end);
-            savedEnd.sys = end->tms_stime;
-            savedEnd.user = end->tms_utime;
         } else if (strcmp(argv[i], "remove_block") == 0) {
             operationNum = 3;
-            savedBegin.real = times(begin);
-            savedBegin.sys = begin->tms_stime;
-            savedBegin.user = begin->tms_utime;
             if (i + 1 >= argc) {
                 printf("Error in input, not enough parameters for creating table");
                 return 0;
@@ -84,14 +73,8 @@ int main(int argc, char **argv) {
             char *tmp;
             int num = (int) strtol(argv[i + 1], &tmp, 0);
             freeBlock(result, num);
-            savedEnd.real = times(end);
-            savedEnd.sys = end->tms_stime;
-            savedEnd.user = end->tms_utime;
         } else if (strcmp(argv[i], "save_block") == 0) {
             operationNum = 4;
-            savedBegin.real = times(begin);
-            savedBegin.sys = begin->tms_stime;
-            savedBegin.user = begin->tms_utime;
             if (i + 1 >= argc) {
                 printf("Error in input, not enough parameters for creating table");
                 return 0;
@@ -103,10 +86,23 @@ int main(int argc, char **argv) {
                 printf("Sorry there were no place for another block\n");
             }
             i = i + 1;
-            savedEnd.real = times(end);
-            savedEnd.sys = end->tms_stime;
-            savedEnd.user = end->tms_utime;
+        } else if (strcmp(argv[i], "save_and_remove") == 0){
+            operationNum = 5;
+            char *tmp;
+            if(i+2 >= argc){
+                return 0;
+            }
+            int num = (int) strtol(argv[i + 2], &tmp, 0);
+
+            for(int j = 0; j < num; j++){
+                freeBlock(result,saveBlock(result, argv[i + 1]));
+            }
+            i = i + 1;
         }
+
+        savedEnd.real = times(end);
+        savedEnd.sys = end->tms_stime;
+        savedEnd.user = end->tms_utime;
         if(operationNum != 0){
             switch (operationNum) {
                 case 1:
@@ -120,6 +116,9 @@ int main(int argc, char **argv) {
                     break;
                 case 4:
                     raport[currentIndex] = "\nSAVE BLOCK\n";
+                    break;
+                case 5:
+                    raport[currentIndex] = "\nSAVING AND REMOVING BLOCK\n";
                     break;
                 default:
                     break;
@@ -140,14 +139,16 @@ int main(int argc, char **argv) {
     saveRaport(raport, currentIndex);
 }
 
+
+
 void saveRaport(char **raport, int size) {
-    FILE* out = fopen("raport2.txt", "w");
+    FILE* out = fopen("raport2.txt", "a");
     if(out == NULL){
         printf("Something went wrong");
         return;
     }
     for(int i = 0; i < size; i++){
-        fprintf(out, "%s",raport[i]);
+        fputs(raport[i], out);
     }
     fclose(out);
 }
