b53f57fa3aaf9d1a5536e9e187ece586601cb1a8
Ajris <hubiflower@gmail.com>
2019-03-11 21:37:26 +0100

    better raports

--- lab1/zad3b/run.sh
+++ lab1/zad3b/run.sh
@@ -7,10 +7,15 @@ saveAndRemoveAlternately="create_table 10 save_and_remove fileMEDIUM.txt 10"
 saveThenRemove="create_table 10 save_block fileSMALL.txt save_block fileMEDIUM.txt save_block fileLARGE.txt remove_block 0 remove_block 1 remove_block 2"
 
 test(){
+    echo "$1 largesearch"
     $1 ${largeSearch}
+    echo "$1 mediumSearch"
     $1 ${mediumSearch}
+        echo "$1 smallSearch"
     $1 ${smallSearch}
+        echo "$1 saveAndRemoveAlternately 1 -> allocating 2-> saving and removing"
     $1 ${saveAndRemoveAlternately}
+        echo "$1 saveThenRemove 1 -> allocating 2,3,4 -> saving 5,6,7 -> removing"
     $1 ${saveThenRemove}
 }
 
