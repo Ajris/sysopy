8450c58f4f30600766f78646050827f0eff8c7a5
Ajris <hubiflower@gmail.com>
2019-03-22 22:11:20 +0100

    added parsing arguments

--- lab3/zad2/main.c
+++ lab3/zad2/main.c
@@ -1,17 +1,32 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
+struct input{
+    char* filename;
+    int time;
+    int type;
+};
+
 void printError(char* message);
+struct input* parseArguments(char** argv);
 
 int main(int argc, char **argv) {
     if(argc != 4){
         printError("Wrong number of arguments");
     }
-    char* filename = argv[1];
-    char* time = argv[2];
-    char* type = argv[3];
-    printf("%s %s %s", filename, time, type);
+    struct input* input = parseArguments(argv);
+
+    printf("%s, %d, %d", input->filename, input->time, input->type);
+
+    free(input);
+}
+
+struct input* parseArguments(char** argv){
+    struct input* input = malloc(sizeof(struct input));
+    input->filename = argv[1];
+    input->time = atoi(argv[2]);
+    input->type = atoi(argv[3]);
+    return input;
 }
 
 void printError(char *message) {
