25f29213c4d3b357e6dd0fb1c92730dbe916a73d
Ajris <hubiflower@gmail.com>
2019-03-22 20:31:39 +0100

    begin of zad2

---
+++ lab3/zad2/main.c
@@ -0,0 +1,15 @@
+#define _XOPEN_SOURCE 500
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ftw.h>
+#include <dirent.h>
+#include <limits.h>
+#include <zconf.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+
+}
