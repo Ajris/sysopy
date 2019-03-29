1dbd1eefe4a719ad3a5404537850a659b1097b83
Ajris <hubiflower@gmail.com>
2019-03-29 13:17:10 +0100

    beginning of lab4

---
+++ lab4/zad1/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printError(char *message);
+
+int main(int argc, char **argv) {
+    printf(1);
+}
+
+void printError(char *message) {
+    printf("%s", message);
+    exit(1);
+}
