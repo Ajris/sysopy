9067dd87e002a2395b1b711e71ef3cc0b92f3199
Ajris <hubiflower@gmail.com>
2019-03-30 19:45:53 +0100

    almost everything works

--- lab4/zad2/main.c
+++ lab4/zad2/main.c
@@ -11,6 +11,7 @@
 #define MAX_FILELINE 100
 
 int numOfFiles = 0;
+int isWorking = 1;
 
 struct input {
     char *filename;
@@ -21,6 +22,7 @@ struct fileData {
     int pid;
     int stopped;
     int repeatingTime;
+    int numOfCopies;
 };
 
 void printError(char *message);
@@ -76,8 +78,17 @@ char *getOnlyFileName(char *filePath) {
     return fileName + 1;
 }
 
+void stop(int signum) {
+    isWorking = 0;
+}
+
+void start(int signum) {
+    isWorking = 1;
+}
+
 void watchFileToMemory(struct fileData *fileData) {
-    int numOfCopies = 0;
+    signal(SIGUSR1, &stop);
+    signal(SIGUSR2, &start);
 
     struct stat fileStats;
     if (lstat(fileData->path, &fileStats) == -1) {
@@ -89,50 +100,109 @@ void watchFileToMemory(struct fileData *fileData) {
 
     char *content = getContent(fileData->path);
     while (1) {
-        if (lstat(fileData->path, &fileStats) == -1) {
-            printf("File: %s", fileData->path);
-            printError("Coudlnt read file");
-        }
-        char *modificationTime = malloc(sizeof(char) * 1000);
-        strftime(modificationTime, 1000, "_%Y-%m-%d_%H-%M-%S", localtime(&fileStats.st_mtime));
-        char *newFileName = malloc(1000 * sizeof(char));
-        sprintf(newFileName, "archiwum/%s%s", getOnlyFileName(fileData->path), modificationTime);
-        if (lastModification < fileStats.st_mtime) {
-            DIR *dir = opendir("archiwum");
-            if (!dir) {
-                mkdir("archiwum", 0777);
+        sleep(1);
+        while (isWorking == 1) {
+
+
+            if (lstat(fileData->path, &fileStats) == -1) {
+                printf("File: %s", fileData->path);
+                printError("Coudlnt read file");
             }
-            FILE *file = fopen(newFileName, "w");
-            if (!file) {
-                printError("Coudlnt created file");
+            char *modificationTime = malloc(sizeof(char) * 1000);
+            strftime(modificationTime, 1000, "_%Y-%m-%d_%H-%M-%S", localtime(&fileStats.st_mtime));
+            char *newFileName = malloc(1000 * sizeof(char));
+            sprintf(newFileName, "archiwum/%s%s", getOnlyFileName(fileData->path), modificationTime);
+            if (lastModification < fileStats.st_mtime) {
+                DIR *dir = opendir("archiwum");
+                if (!dir) {
+                    mkdir("archiwum", 0777);
+                }
+                FILE *file = fopen(newFileName, "w");
+                if (!file) {
+                    printError("Coudlnt created file");
+                }
+                fwrite(content, 1, strlen(content), file);
+                fclose(file);
+                lastModification = fileStats.st_mtime;
+                content = getContent(fileData->path);
+                fileData->numOfCopies++;
+                closedir(dir);
             }
-            fwrite(content, 1, strlen(content), file);
-            fclose(file);
-            lastModification = fileStats.st_mtime;
-            content = getContent(fileData->path);
-            numOfCopies++;
-            closedir(dir);
+            free(modificationTime);
+            free(newFileName);
+            sleep(fileData->repeatingTime);
         }
-        free(modificationTime);
-        free(newFileName);
-        sleep(fileData->repeatingTime);
     }
     free(content);
+    exit(fileData->numOfCopies);
 }
 
-void monitorEverything(struct fileData** fileData){
+void monitorEverything(struct fileData **fileData) {
     while (1) {
         char *value = malloc(sizeof(char) * 64);
-        fgets(value, 10, stdin);
+        fgets(value, 20, stdin);
         if (strcmp(value, "END\n") == 0) {
             for (int i = 0; i < numOfFiles; i++) {
-                int num = kill(fileData[i]->pid, SIGKILL);
-                printf("Process %d created %s.\n", fileData[i]->pid, fileData[i]->path);
-                printf("Num %d\n", num);
+                kill(fileData[i]->pid, SIGKILL);
+                printf("Process %d file %s.\n", fileData[i]->pid, fileData[i]->path);
             }
             free(value);
             exit(1);
+        } else if (strcmp(value, "LIST\n") == 0) {
+            for (int i = 0; i < numOfFiles; i++) {
+                printf("PROCESS: %d || FILE: %s || IS STOPPED:%d\n", fileData[i]->pid, fileData[i]->path,
+                       fileData[i]->stopped);
+            }
+        } else if (strcmp(value, "STOP ALL\n") == 0) {
+            for (int i = 0; i < numOfFiles; i++) {
+                kill(fileData[i]->pid, SIGUSR1);
+                fileData[i]->stopped = 1;
+            }
+        } else if (strcmp(value, "START ALL\n") == 0) {
+            for (int i = 0; i < numOfFiles; i++) {
+                kill(fileData[i]->pid, SIGUSR2);
+                fileData[i]->stopped = 0;
+            }
+        } else if (strncmp(value, "STOP", 4) == 0) {
+            strtok(value, " ");
+            char *ptr = strtok(NULL, " ");
+            if (ptr != NULL) {
+                int pid = atoi(ptr);
+                int found = 0;
+                for (int i = 0; i < numOfFiles; i++) {
+                    if (fileData[i]->pid == pid) {
+                        fileData[i]->stopped = 1;
+                        kill(pid, SIGUSR1);
+                        found = 1;
+                    }
+                }
+                if (!found) {
+                    printf("Couldn't find that pid\n");
+                }
+            } else {
+                printf("You didnt specified pid");
+            }
+        } else if (strncmp(value, "START", 5) == 0) {
+            strtok(value, " ");
+            char *ptr = strtok(NULL, " ");
+            if (ptr != NULL) {
+                int pid = atoi(ptr);
+                int found = 0;
+                for (int i = 0; i < numOfFiles; i++) {
+                    if (fileData[i]->pid == pid) {
+                        fileData[i]->stopped = 0;
+                        kill(pid, SIGUSR2);
+                        found = 1;
+                    }
+                }
+                if (!found) {
+                    printf("Couldn't find that pid\n");
+                }
+            } else {
+                printf("You didnt specified pid");
+            }
         }
+        free(value);
     }
 }
 
