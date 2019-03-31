40e0ca536e58fbbb559118d468fe9a0eeb6a2465
Ajris <hubiflower@gmail.com>
2019-03-31 20:33:51 +0200

    should work

--- lab4/zad3/catcher.c
+++ lab4/zad3/catcher.c
@@ -1,11 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+int signalsReceived = 0;
+char *mode;
 
 void printError(char *message);
 
-int main(int argc, char **argv) {
+void blockSignals();
 
-    printf("1");
+void addHandlers();
+
+void handleEverything(int sig, siginfo_t *info, void *ucontext);
+
+int main(int argc, char **argv) {
+    if (argc != 2)
+        printError("Wrong number of arguments");
+    printf("Catcher PID: %d", getpid());
+    mode = argv[1];
+    blockSignals();
+    addHandlers();
+    while (1);
+}
+
+void handleEverything(int sig, siginfo_t *info, void *ucontext) {
+    if (sig == SIGUSR1 || sig == SIGRTMIN) {
+        signalsReceived++;
+    } else if (sig == SIGUSR2 || sig == SIGRTMAX) {
+        int processNum = info->si_pid;
+        if (strcmp(mode, "KILL") == 0) {
+            for (int i = 0; i < signalsReceived; i++)
+                kill(processNum, SIGUSR1);
+            kill(processNum, SIGUSR2);
+        } else if (strcmp(mode, "SIGQUEUE") == 0) {
+            union sigval justToBeHere;
+            for (int i = 0; i < signalsReceived; i++)
+                sigqueue(processNum, SIGUSR1, justToBeHere);
+            sigqueue(processNum, SIGUSR2, justToBeHere);
+        } else if (strcmp(mode, "SIGRT") == 0) {
+            for (int i = 0; i < signalsReceived; i++)
+                kill(processNum, SIGRTMIN);
+            kill(processNum, SIGRTMAX);
+        } else {
+            printError("Sth went wrong");
+        }
+        printf("Catcher got: %d\n", signalsReceived);
+        exit(1);
+    }
+}
+
+void addHandlers() {
+    struct sigaction *handlerInfos = malloc(sizeof(struct sigaction));
+    handlerInfos->sa_flags = SA_SIGINFO;
+    handlerInfos->sa_sigaction = handleEverything;
+    sigemptyset(&handlerInfos->sa_mask);
+    if (strcmp(mode, "KILL") == 0) {
+        sigaddset(&handlerInfos->sa_mask, SIGUSR1);
+        sigaddset(&handlerInfos->sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, handlerInfos, NULL);
+        sigaction(SIGUSR2, handlerInfos, NULL);
+    } else if (strcmp(mode, "SIGQUEUE") == 0) {
+        sigaddset(&handlerInfos->sa_mask, SIGUSR1);
+        sigaddset(&handlerInfos->sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, handlerInfos, NULL);
+        sigaction(SIGUSR2, handlerInfos, NULL);
+    } else if (strcmp(mode, "SIGRT") == 0) {
+        sigaddset(&handlerInfos->sa_mask, SIGRTMIN);
+        sigaddset(&handlerInfos->sa_mask, SIGRTMAX);
+        sigaction(SIGRTMIN, handlerInfos, NULL);
+        sigaction(SIGRTMAX, handlerInfos, NULL);
+    } else {
+        printError("wrong mode");
+    }
+}
+
+void blockSignals() {
+    sigset_t *allSignalsToBlock = malloc(sizeof(sigset_t));
+    sigfillset(allSignalsToBlock);
+    if (strcmp(mode, "KILL") == 0) {
+        sigdelset(allSignalsToBlock, SIGUSR1);
+        sigdelset(allSignalsToBlock, SIGUSR2);
+    } else if (strcmp(mode, "SIGQUEUE") == 0) {
+        sigdelset(allSignalsToBlock, SIGUSR1);
+        sigdelset(allSignalsToBlock, SIGUSR2);
+    } else if (strcmp(mode, "SIGRT") == 0) {
+        sigdelset(allSignalsToBlock, SIGRTMIN);
+        sigdelset(allSignalsToBlock, SIGRTMAX);
+    } else {
+        printError("wrong mode");
+    }
+    sigprocmask(SIG_BLOCK, allSignalsToBlock, NULL);
 }
 
 void printError(char *message) {
--- lab4/zad3/sender.c
+++ lab4/zad3/sender.c
@@ -1,28 +1,110 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 
+int signalsReceived = 0;
+int numOfSignals;
+
 void printError(char *message);
+
+void blockSignals(char *mode);
+
+void addHandlers(char *mode);
+
+void handleEverything(int sig, siginfo_t *info, void *ucontext);
+
+void sendSignals(char *mode, int numOfSignals, int catcherPID);
 
 int main(int argc, char **argv) {
     if (argc != 4)
         printError("Wrong number of arguments");
 
-    int numOfSignals = atoi(argv[1]);
+    numOfSignals = atoi(argv[1]);
     int catcherPID = atoi(argv[2]);
     char *mode = argv[3];
 
-    if(strcmp(mode, "KILL") == 0){
+    if (numOfSignals <= 0)
+        printError("Wrong num of signals");
+    if (catcherPID <= 0)
+        printError("Wrong num of catcherpid");
 
-    } else if(strcmp(mode, "SIGQUEUE") == 0){
+    blockSignals(mode);
+    sendSignals(mode, numOfSignals, catcherPID);
+    addHandlers(mode);
+    while (1);
+}
 
-    } else if(strcmp(mode, "SIGRT") == 0){
+void sendSignals(char *mode, int numOfSignals, int catcherPID) {
+    if (strcmp(mode, "KILL") == 0) {
+        for (int i = 0; i < numOfSignals; i++)
+            kill(catcherPID, SIGUSR1);
+        kill(catcherPID, SIGUSR2);
+    } else if (strcmp(mode, "SIGQUEUE") == 0) {
+        union sigval justToBeHere;
+        for (int i = 0; i < numOfSignals; i++)
+            sigqueue(catcherPID, SIGUSR1, justToBeHere);
+        sigqueue(catcherPID, SIGUSR2, justToBeHere);
+    } else if (strcmp(mode, "SIGRT") == 0) {
+        for (int i = 0; i < numOfSignals; i++)
+            kill(catcherPID, SIGRTMIN);
+        kill(catcherPID, SIGRTMAX);
+    } else {
+        printError("Wrong mode");
+    }
+}
 
+void handleEverything(int sig, siginfo_t *info, void *ucontext) {
+    if (sig == SIGUSR1 || sig == SIGRTMIN) {
+        signalsReceived++;
+    } else if (sig == SIGUSR2 || sig == SIGRTMAX) {
+        printf("Sent: %d Got: %d\n", numOfSignals, signalsReceived);
+        exit(1);
+    }
+}
+
+void addHandlers(char *mode) {
+    struct sigaction *act = malloc(sizeof(struct sigaction));
+    act->sa_flags = SA_SIGINFO;
+    act->sa_sigaction = handleEverything;
+    sigemptyset(&act->sa_mask);
+    if (strcmp(mode, "KILL") == 0) {
+        sigaddset(&act->sa_mask, SIGUSR1);
+        sigaddset(&act->sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, act, NULL);
+        sigaction(SIGUSR2, act, NULL);
+    } else if (strcmp(mode, "SIGQUEUE") == 0) {
+        sigaddset(&act->sa_mask, SIGUSR1);
+        sigaddset(&act->sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, act, NULL);
+        sigaction(SIGUSR2, act, NULL);
+    } else if (strcmp(mode, "SIGRT") == 0) {
+        sigaddset(&act->sa_mask, SIGRTMIN);
+        sigaddset(&act->sa_mask, SIGRTMAX);
+        sigaction(SIGRTMIN, act, NULL);
+        sigaction(SIGRTMAX, act, NULL);
     } else {
         printError("wrong mode");
     }
+}
+
+
+void blockSignals(char *mode) {
+    sigset_t *allSignalsToBlock = malloc(sizeof(sigset_t));
+    sigfillset(allSignalsToBlock);
+    if (strcmp(mode, "KILL") == 0) {
+        sigdelset(allSignalsToBlock, SIGUSR1);
+        sigdelset(allSignalsToBlock, SIGUSR2);
+    } else if (strcmp(mode, "SIGQUEUE") == 0) {
+        sigdelset(allSignalsToBlock, SIGUSR1);
+        sigdelset(allSignalsToBlock, SIGUSR2);
+    } else if (strcmp(mode, "SIGRT") == 0) {
+        sigdelset(allSignalsToBlock, SIGRTMIN);
+        sigdelset(allSignalsToBlock, SIGRTMAX);
+    } else {
+        printError("wrong mode");
+    }
+    sigprocmask(SIG_BLOCK, allSignalsToBlock, NULL);
 }
 
 void printError(char *message) {
