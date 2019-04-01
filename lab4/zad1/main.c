aeae3b8be9758b06cb1611f8bb4330df329b0407
Ajris <hubiflower@gmail.com>
2019-04-01 23:50:03 +0200

    small fix

--- lab4/zad1/main.c
+++ lab4/zad1/main.c
@@ -21,14 +21,16 @@ int main(int argc, char **argv) {
     time_t t;
     signal(SIGINT, ctrlC_Handler);
     while(1){
-        time(&t);
-        printf("%s\n", ctime(&t));
+        if(value == 0){
+            time(&t);
+            printf("%s\n", ctime(&t));
+        }
         sleep(1);
     }
 }
 void ctrlZ_Handler(int sig, siginfo_t *siginfo, void *context){
     if(value == 0){
-        printf("Oczekuję na CTRL+Z - kontynuacja albo CTR+C - zakończenie programu");
+        printf("Oczekuję na CTRL+Z - kontynuacja albo CTR+C - zakończenie programu\n");
         value++;
     } else {
         value = 0;
