97e23b2c697282cc698feb561926cda4ee631f74
Ajris <hubiflower@gmail.com>
2019-04-07 02:19:31 +0200

    not working

--- lab5/zad1/main.c
+++ lab5/zad1/main.c
@@ -13,7 +13,9 @@
 
 void printError(char *message);
 
-int getNumOfLines(char *string);
+int getNumOfLines(char *wholeFile);
+
+char **getLines(char *wholeFile, int numOfLines);
 
 int main(int argc, char **argv) {
     if (argc != 2)
@@ -22,32 +24,58 @@ int main(int argc, char **argv) {
     if (file == NULL)
         printError("Couldn't open file");
 
-    fseek(file, 0 , SEEK_END);
+    fseek(file, 0, SEEK_END);
     size_t fileLength = ftell(file);
-    fseek(file, 0 , SEEK_SET);
+    fseek(file, 0, SEEK_SET);
 
     char *wholeFile = malloc(fileLength + 1);
 
-    if(fread(wholeFile, 1, fileLength, file) != fileLength)
+    if (fread(wholeFile, 1, fileLength, file) != fileLength)
         printError("Error while reading file");
 
     printf("%s\n", wholeFile);
 
     int numOfLines = getNumOfLines(wholeFile);
-    printf("%d", numOfLines);
+
+    printf("%s\n", wholeFile);
+
+    printf("%d\n", numOfLines);
+    char **lines = getLines(wholeFile, numOfLines);
+
+    for (int i = 0; i < numOfLines; i++) {
+        printf("LINE %d:%s\n", i, lines[i]);
+    }
 
     fclose(file);
 }
 
-int getNumOfLines(char *string) {
+char **getLines(char *wholeFile, int numOfLines) {
+    char **lines = malloc(numOfLines * sizeof(char *));
+    for (int i = 0; i < numOfLines; i++) {
+        lines[i] = malloc(MAX_LINE_LENGTH * sizeof(char));
+    }
+
+    char *copy = malloc(strlen(wholeFile) * sizeof(char));
+    strcpy(copy, wholeFile);
+    lines[0] = strtok(copy, "\n");
+    int i = 1;
+    while ((lines[i++] = strtok(NULL, "\n"))) {
+
+    }
+    free(copy);
+    return lines;
+}
+
+int getNumOfLines(char *wholeFile) {
     int noOfTokens = 1;
-    char* stringCpy = malloc(strlen(string) * sizeof(char));
-    strcpy(stringCpy, string);
-    strtok(stringCpy, "\n");
-    while(strtok(NULL, "\n")) {
+    char *copy = malloc(strlen(wholeFile) * sizeof(char));
+    strcpy(copy, wholeFile);
+    strdup(copy);
+    strtok(copy, "\n");
+    while (strtok(NULL, "\n")) {
         noOfTokens++;
     }
-    free(stringCpy);
+    free(copy);
     return noOfTokens;
 }
 
