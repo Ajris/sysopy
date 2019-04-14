a86ba54368a9fbd2573e8f8c7eb2dfaf416053cf
Ajris <hubiflower@gmail.com>
2019-04-14 18:55:44 +0200

    zad8

--- kol/zad8/main.c
+++ kol/zad8/main.c
@@ -1,38 +1,75 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <wait.h>
+#include <stdlib.h>
+
+#include <sys/times.h>
+
+static void display_times(clock_t, struct tms *, struct tms *);
+
+static void do_cmd(char *, int instruction, int final);
+
 
 int main(int argc, char *argv[]) {
-    int i, pid;
+    int i;
 
-    if (argc != 2) {
-        printf("Not a suitable number of program arguments");
-        exit(2);
-    } else {
-        for (i = 0; i < atoi(argv[1]); i++) {
-            pid = fork();
-            if(pid < 0){
-                fprintf(stderr, "ERROR");
-                exit(1);
-            } else if(pid == 0){
-                printf("I am %d PID %d PARENTPID %d\n", i,getpid(), getppid());
-                sleep(10);
-                exit(0);
-            }
-            //*********************************************************
-            //Uzupelnij petle w taki sposob aby stworzyc dokladnie argv[1] procesow potomnych, bedacych dziecmi
-            //   tego samego procesu macierzystego.
-            // Kazdy proces potomny powinien:
-            // - "powiedziec ktorym jest dzieckiem",
-            //-  jaki ma pid,
-            //- kto jest jego rodzicem
-            //******************************************************
-        }
+    setbuf(stdout, NULL);
+    for (i = 1; i < argc; i++)
+        do_cmd(argv[i], i, argc - 1);
+    exit(0);
+}
 
-        for(int i = 0; i < atoi(argv[1]); i++){
-            wait(NULL);
-        }
+static void do_cmd(char *cmd, int instruction, int final) {
+
+/* wykonaj program, policz jego czasy, sprawdz, czy to nie jest ostatni program */
+    struct tms tmsstart, tmsend;
+    clock_t start, end;
+    int status;
+
+    printf("\ncommand: %s\n", cmd);
+
+
+
+    if ((start = times(&tmsstart)) == -1)           /* zapamietaj wartosci poczatkowe pomiaru czasu*/
+    {
+        printf("times error");
+        exit(-1);
     }
-    return 0;
+
+
+    if ((status = system(cmd)) < 0)         /* wykonaj polecenie */
+    {
+        printf("system() error");
+        exit(-1);
+    }
+
+    if ((end = times(&tmsend)) == -1)      /* zapamietaj wartosci koncowe */
+    {
+        printf("times error");
+        exit(-1);
+    }
+
+    display_times(end - start, &tmsstart, &tmsend);
+    if (instruction == final)
+        exit(status);
+}
+
+static void display_times(clock_t real, struct tms *tmsstart, struct tms *tmsend) {
+    static long clktck = 0;
+
+    if (clktck == 0)   /* sprawdz, czy stan zmiennej odczytywany pierwszy raz */
+        if ((clktck = sysconf(_SC_CLK_TCK)) < 0) {
+            printf("sysconf error");
+            exit(-1);
+        }
+
+    printf("  real:  %7.2f\n", real / (double) clktck);
+    printf("  user:  %7.2f\n",
+           (tmsend->tms_utime - tmsstart->tms_utime) / (double) clktck);
+    printf("  sys:   %7.2f\n",
+           (tmsend->tms_stime - tmsstart->tms_stime) / (double) clktck);
+    printf("  child user:  %7.2f\n",
+           (tmsend->tms_cutime - tmsstart->tms_cutime) / (double) clktck);
+    printf("  child sys:   %7.2f\n",
+           (tmsend->tms_cstime - tmsstart->tms_cstime) / (double) clktck);
 }
