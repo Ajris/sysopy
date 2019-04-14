e133d5df835eca9a9fe54638310f7e4394f49f3d
Ajris <hubiflower@gmail.com>
2019-04-14 17:06:08 +0200

    finished zad3

--- kol/zad3/main.c
+++ kol/zad3/main.c
@@ -2,36 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) {
-	int pdesk[2];
-	if (pipe(pdesk) == -1){
-		perror("PIPE");
-		return 1;
-	}
+int main(int argc, char *argv[]) {
+    int pdesk[2];
+    if (pipe(pdesk) == -1) {
+        perror("PIPE");
+        return 1;
+    }
 
-	switch(fork()){
-	  case -1:
-		perror("FORK");
-		return 2;
-	  case 0:
-		dup2(pdesk[1], STDOUT_FILENO);
-		execvp("ls", argv);
-		perror("EXECVP ls");
-		exit(1);
-	  default: {
-   /* Zamknij deskryptor do zapisu,
-    * skopiuj deskyptor końca do odczytu tego potoku na deskryptor wejścia standardowego,
-    * wykonaj  tr "a-z" "A-Z", w przypadku błędu  obsłuż go i wyjdź, zwracając 3.
-   */
+    switch (fork()) {
+        case -1:
+            perror("FORK");
+            return 2;
+        case 0:
+            dup2(pdesk[1], STDOUT_FILENO);
+            execvp("ls", argv);
+            perror("EXECVP ls");
+            exit(1);
+        default: {
+            /* Zamknij deskryptor do zapisu,
+             * skopiuj deskyptor końca do odczytu tego potoku na deskryptor wejścia standardowego,
+             * wykonaj  tr "a-z" "A-Z", w przypadku błędu  obsłuż go i wyjdź, zwracając 3.
+            */
+            close(pdesk[1]);
+            dup2(pdesk[0], STDIN_FILENO);
+            if (execlp("tr", "tr", "a-z", "A-Z", NULL) < 0) {
+                printf("Something went wrong");
+                return 3;
+            }
 
-
-
-
-
-
-
-   /* koniec */
-       	 }
-	}
-   return 0;
-   }
+            /* koniec */
+        }
+    }
+    return 0;
+}
