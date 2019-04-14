19b626d171b6ac738aaea5584f81ee781496da57
Ajris <hubiflower@gmail.com>
2019-04-14 16:32:17 +0200

    small fix for zad

--- kol/zad1/main.c
+++ kol/zad1/main.c
@@ -11,6 +11,10 @@ int main(int argc, char *argv[]) {
     }
     struct dirent *pozycja;
 
+    /*Otwórz katalog, w przypadku błędu otwarcia zwróć błąd funkcji otwierającej i zwrócć 1.
+    Wyświetl zawartość katalogu katalog, pomijając "." i ".."
+    Jeśli podczas tej operacji wartość errno zostanie ustawiona, zwróć błąd funkcji czytającej oraz wartość 2. */
+
     katalog = opendir(argv[1]);
 
     if (katalog == NULL) {
@@ -23,19 +27,12 @@ int main(int argc, char *argv[]) {
             printf("%s\n", pozycja->d_name);
         }
     }
-    if(errno != 0){
+    if (errno != 0) {
         fprintf(stderr, "%s", strerror(errno));
         return 2;
     }
 
-
-/*Otwórz katalog, w przypadku błędu otwarcia zwróć błąd funkcji otwierającej i zwrócć 1. 
-Wyświetl zawartość katalogu katalog, pomijając "." i ".."
-Jeśli podczas tej operacji wartość errno zostanie ustawiona, zwróć błąd funkcji czytającej oraz wartość 2. */
-
-
-
-/*koniec*/
+    /*koniec*/
     closedir(katalog);
     return (0);
 }
--- kol/zad2/main.c
+++ kol/zad2/main.c
@@ -5,35 +5,26 @@
 #include <stdio.h>
 #include <signal.h>
 
-int main()
-{
-  pid_t child;
-  int status, retval;
-  if((child = fork()) < 0) {
-    perror("fork");
-    exit(EXIT_FAILURE);
-  }
-  if(child == 0) {
-    sleep(100);
+int main() {
+    pid_t child;
+    int status, retval;
+    if ((child = fork()) < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (child == 0) {
+        sleep(100);
+        exit(EXIT_SUCCESS);
+    } else {
+    /* Proces macierzysty pobiera status  zakończenie potomka child,
+     * nie zawieszając swojej pracy. Jeśli proces się nie zakończył, wysyła do dziecka sygnał SIGKILL.
+     * Jeśli wysłanie sygnału się nie powiodło, ponownie oczekuje na zakończenie procesu child,
+     * tym razem zawieszając pracę do czasu zakończenia sygnału
+     * jeśli się powiodło, wypisuje komunikat sukcesu zakończenia procesu potomka z numerem jego PID i statusem zakończenia. */
+
+
+
+    /* koniec*/
+    } //else
     exit(EXIT_SUCCESS);
-  }
-  else {
-/* Proces macierzysty pobiera status  zakończenie potomka child,
- * nie zawieszając swojej pracy. Jeśli proces się nie zakończył, wysyła do dziecka sygnał SIGKILL.
- * Jeśli wysłanie sygnału się nie powiodło, ponownie oczekuje na zakończenie procesu child,
- * tym razem zawieszając pracę do czasu zakończenia sygnału
- * jeśli się powiodło, wypisuje komunikat sukcesu zakończenia procesu potomka z numerem jego PID i statusem zakończenia. */
-
-
-
-
-
-
-
-
-
-
-/* koniec*/ 
- } //else
-  exit(EXIT_SUCCESS);
 }
