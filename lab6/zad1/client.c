65bfa2a1e96a66bb77777e4cfb5cfa2b1d65197c
Ajris <hubiflower@gmail.com>
2019-04-28 20:45:06 +0200

    small changes to files

--- lab6/zad1/client.c
+++ lab6/zad1/client.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
 
 int main(int argc, char **argv) {
 
---
+++ lab6/zad2/client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+
+int main(int argc, char **argv) {
+
+}
