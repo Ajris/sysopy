39021344a85c40d5687db78f59c288e567382e8e
Ajris <hubiflower@gmail.com>
2019-05-12 19:32:47 +0200

    Added basic programs

---
+++ lab7/zad1/loader.c
@@ -0,0 +1,8 @@
+//
+// Created by ajris on 12.05.19.
+//
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+}
---
+++ lab7/zad2/trucker.c
@@ -0,0 +1,9 @@
+//
+// Created by ajris on 12.05.19.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+}
