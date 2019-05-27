bd4a3e88380df8a44f1ba2639d69bde0dc9ee0de
Ajris <hubiflower@gmail.com>
2019-05-27 14:24:31 +0200

    finished lab9

--- lab9/zad1/info.h
+++ lab9/zad1/info.h
@@ -16,19 +16,34 @@
 #include <mqueue.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/time.h>
 
 typedef struct Passenger{
     int id;
+    int cart;
 } Passenger;
 
 typedef struct Cart{
     int id;
     int size;
     int raids;
+    Passenger* passIds;
 } Cart;
 
+int cartsCount;
+int cartSize;
 pthread_cond_t* cartsCond;
 pthread_mutex_t* cartsMutex;
-int* cardOrder;
+
+pthread_mutex_t stationMutex;
+pthread_mutex_t passengerMutex;
+pthread_mutex_t emptyCartMutex;
+pthread_mutex_t fullCartMutex;
+
+pthread_cond_t emptyCondition;
+pthread_cond_t fullCondition;
+
+Cart* carts;
+int stationCartID;
 
 #endif //LAB9_ZAD1_INFO_H
--- lab9/zad1/main.c
+++ lab9/zad1/main.c
@@ -6,48 +6,168 @@
 
 void printError(char *message);
 
-void runPassenger(void* data);
+void *runPassenger(void *data);
 
-void runCart(void* data);
+void *runCart(void *data);
+
+struct timeval getCurrentTime();
 
 int main(int argc, char **argv) {
-    if(argc != 5)
+    if (argc != 5)
         printError("Wrong num of arguments");
     int passengersCount = atoi(argv[1]);
-    int cartsCount = atoi(argv[2]);
-    int cartSize = atoi(argv[3]);
+    cartsCount = atoi(argv[2]);
+    cartSize = atoi(argv[3]);
     int raidCount = atoi(argv[4]);
 
-    if(passengersCount <= 0 || cartsCount <= 0 || cartSize <= 0 || raidCount <= 0)
+    if (passengersCount <= 0 || cartsCount <= 0 || cartSize <= 0 || raidCount <= 0)
         printError("Error arguemnts");
 
+    stationCartID = 0;
     pthread_t passThr[passengersCount];
     pthread_t cartThr[cartsCount];
     Passenger passengers[passengersCount];
-    Cart carts[cartsCount];
+    carts = malloc(sizeof(Cart) * cartsCount + sizeof(int) * passengersCount);
+    cartsMutex = malloc(sizeof(pthread_mutex_t) * cartsCount);
+    cartsCond = malloc(sizeof(pthread_cond_t) * cartsCount);
 
-    for(int i = 0; i < passengersCount; i++){
+    pthread_mutex_init(&stationMutex, NULL);
+    pthread_mutex_init(&emptyCartMutex, NULL);
+    pthread_mutex_init(&passengerMutex, NULL);
+    pthread_mutex_init(&fullCartMutex, NULL);
+    pthread_cond_init(&emptyCondition, NULL);
+    pthread_cond_init(&fullCondition, NULL);
+
+    for (int i = 0; i < passengersCount; i++) {
         passengers[i].id = i;
+        passengers[i].cart = -1;
+    }
+
+    for (int i = 0; i < cartsCount; i++) {
+        carts[i].id = i;
+        carts[i].size = 0;
+        carts[i].raids = raidCount;
+        carts[i].passIds = malloc(sizeof(Passenger) * cartSize);
+        pthread_mutex_init(&cartsMutex[i], NULL);
+        pthread_cond_init(&cartsCond[i], NULL);
+    }
+
+    for (int i = 0; i < cartsCount; i++) {
+        pthread_create(&cartThr[i], NULL, runCart, &carts[i]);
+    }
+
+    for (int i = 0; i < passengersCount; i++) {
         pthread_create(&passThr[i], NULL, runPassenger, &passengers[i]);
     }
 
+    for (int i = 0; i < cartsCount; i++) {
+        pthread_join(cartThr[i], NULL);
+    }
+
+    for (int i = 0; i < cartsCount; i++) {
+        pthread_mutex_destroy(&cartsMutex[i]);
+    }
     for(int i = 0; i < cartsCount; i++){
-        carts[i].id = i;
-        carts[i].size = cartSize;
-        carts[i].raids = raidCount;
-        pthread_mutex_init(&cartsMutex[i], NULL);
-        pthread_cond_init(&cartsCond[i], NULL);
-        pthread_create(&cartThr[i], NULL, runCart, &carts[i]);
+        free(carts[i].passIds);
+    }
+    free(carts);
+    free(cartsMutex);
+    free(cartsCond);
+}
+
+void *runPassenger(void *data) {
+    Passenger *passenger = (Passenger *) data;
+    while (1) {
+        pthread_mutex_lock(&passengerMutex);
+
+        passenger->cart = stationCartID;
+        carts[stationCartID].passIds[carts[stationCartID].size] = *passenger;
+        carts[stationCartID].size = carts[stationCartID].size + 1;
+        struct timeval curr = getCurrentTime();
+        printf("PASSENGER ENTERED || ID: %d PEOPLE IN CART: %d TIME: %ld.%06ld \n", passenger->id,carts[stationCartID].size, curr.tv_sec, curr.tv_usec);
+
+        if(carts[stationCartID].size == cartSize){
+            srand(time(NULL));
+            curr = getCurrentTime();
+            printf("PASSENGER PRESSED START || ID: %d PEOPLE IN CART: %d TIME: %ld.%06ld\n", carts[stationCartID].passIds[rand()%carts->size].id,carts[stationCartID].size, curr.tv_sec, curr.tv_usec);
+            pthread_cond_signal(&fullCondition);
+            pthread_mutex_unlock(&fullCartMutex);
+        } else {
+            pthread_mutex_unlock(&passengerMutex);
+        }
+
+        pthread_mutex_lock(&cartsMutex[passenger->cart]);
+        curr = getCurrentTime();
+        carts[stationCartID].size--;
+        printf("PASSENGER LEFT || ID: %d PEOPLE IN CART: %d TIME: %ld.%06ld \n", passenger->id, carts[stationCartID].size,curr.tv_sec, curr.tv_usec);
+        if(carts[stationCartID].size == 0){
+            pthread_cond_signal(&emptyCondition);
+            pthread_mutex_unlock(&emptyCartMutex);
+        }
+        pthread_mutex_unlock(&cartsMutex[passenger->cart]);
+        passenger->cart = -1;
     }
 }
 
-void runPassenger(void* data){
-    Passenger* pass = (Passenger*) data;
-    while()
+void *runCart(void *data) {
+    Cart *cart = (Cart *) data;
+    if (cart->id == 0)
+        pthread_mutex_lock(&passengerMutex);
+
+    for (int i = 0; i < cart->raids; i++) {
+        pthread_mutex_lock(&stationMutex);
+        if (cart->id != stationCartID) {
+            pthread_cond_wait(&cartsCond[cart->id], &stationMutex);
+        }
+        struct timeval curr = getCurrentTime();
+        printf("CART COME || ID: %d TIME: %ld.%06ld\n", cart->id, curr.tv_sec, curr.tv_usec);
+
+        if (i != 0) {
+            pthread_mutex_unlock(&cartsMutex[cart->id]);
+            pthread_cond_wait(&emptyCondition, &emptyCartMutex);
+        }
+
+        pthread_mutex_lock(&cartsMutex[cart->id]);
+        pthread_mutex_unlock(&passengerMutex);
+        pthread_cond_wait(&fullCondition, &fullCartMutex);
+
+        curr = getCurrentTime();
+        printf("CART FULL || ID: %d TIME: %ld.%06ld\n", cart->id, curr.tv_sec, curr.tv_usec);
+        stationCartID = (stationCartID + 1) % cartsCount;
+
+        pthread_cond_signal(&cartsCond[stationCartID]);
+        pthread_mutex_unlock(&stationMutex);
+    }
+
+    pthread_mutex_lock(&stationMutex);
+
+    if(cart->id != stationCartID) {
+        pthread_cond_wait(&cartsCond[cart->id], &stationMutex);
+    }
+
+    struct timeval curr = getCurrentTime();
+    printf("CART COME || ID: %d TIME: %ld.%06ld\n", cart->id, curr.tv_sec, curr.tv_usec);
+
+    stationCartID = cart->id;
+
+    pthread_mutex_unlock(&cartsMutex[cart->id]);
+    pthread_cond_wait(&emptyCondition,&emptyCartMutex);
+
+    stationCartID = (stationCartID + 1)%cartsCount;
+
+    curr = getCurrentTime();
+    printf("CART FINISHED || ID: %d TIME: %ld.%06ld\n", cart->id, curr.tv_sec, curr.tv_usec);
+
+    pthread_cond_signal(&cartsCond[stationCartID]);
+    pthread_mutex_unlock(&stationMutex);
+
+    pthread_exit(NULL);
 }
 
-void runCart(void* cart){
-
+struct timeval getCurrentTime() {
+    struct timeval curr;
+    gettimeofday(&curr, NULL);
+    return curr;
 }
 
 void printError(char *message) {
