41c64d1466c76581441442d4012250c7a8a7411b
Ajris <hubiflower@gmail.com>
2019-06-04 00:36:32 +0200

    zad15

--- kol2/zad15/main.c
+++ kol2/zad15/main.c
@@ -6,14 +6,17 @@
 #include <strings.h>
 #include <unistd.h>
 
-
 /*
  * Funkcja 'uname' przyjmuje socket domeny unix ('s') i jego
  * docelowa nazwe ('name'). Funkcja 'uname' powinna:
  *   - nadac socketowi 's' nazwe 'name',
  *   - zwrocic 0 w przypadku powodzenia i -1 w przypadku bledu.
  */
-int uname(int s, char *name){
+int uname(int s, char *name) {
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    sprintf(addr.sun_path, "%s", name);
+    return bind(s, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
     // Uzupelnij cialo funkcji uname zgodnie z
     // komentarzem powyzej.
 }
@@ -24,28 +27,34 @@ int uname(int s, char *name){
  *   - polaczyc socket 's' z adresem 'name',
  *   - zwrocic 0 w przypadku powodzenia i -1 w przypadku bledu.
  */
-int uconnect(int s, char *name){
+int uconnect(int s, char *name) {
     // Uzupelnij cialo funkcji uconnect zgodnie z
     // komentarzem powyzej
+
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    sprintf(addr.sun_path, "%s", name);
+    return connect(s, (const struct sockaddr *) &addr, sizeof(addr));
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) return -1;
-    if(fork() > 0) {
-        char buf[4];
+    if (fork() > 0) {
+        char buf[5];
         int s = socket(AF_UNIX, SOCK_STREAM, 0);
-        if(uname(s, argv[1]) || listen(s, 1)) return -1;
+        if (uname(s, argv[1]) || listen(s, 1)) return -1;
         int fd = accept(s, NULL, 0);
-        read(fd, buf, 4);
+        read(fd, buf, 5);
         printf("%4s\n", buf);
     } else {
-        char buf[4];
+        char buf[5];
         int i;
-        for(i=0;i<4;i++) buf[i]=(rand()%26+'b');
+        for (i = 0; i < 4; i++) buf[i] = ('b');
         int c = socket(AF_UNIX, SOCK_STREAM, 0);
+        buf[4] = '\0';
         sleep(1);
-        if(uconnect(c, argv[1])) return -1;
-        write(c, buf, 4);
+        if (uconnect(c, argv[1])) return -1;
+        write(c, buf, 5);
     }
 
     return 0;
