6a1f4e9b1eb7acedf069c2f80306facb205ad59e
Ajris <hubiflower@gmail.com>
2019-06-03 22:12:18 +0200

    finished zad9

--- kol2/zad9/receiver.c
+++ kol2/zad9/receiver.c
@@ -6,45 +6,40 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define KEY  "/home/ajris"
 
-#define KEY  "./queuekey"
-
+struct mess {
+    long mtype;
+    long value;
+};
 
 int main() {
-        sleep(1);
-        int val = 0;
-
-
-	/**********************************
-	Otworz kolejke systemu V "reprezentowana" przez KEY
-	**********************************/
-    key_t k = ftok(KEY, 1);
-    int q = msgget(k, IPC_CREAT | 0644);
-    if(q < 0) {
-    perror("msgget");
-    exit(1);
+    sleep(1);
+    /**********************************
+    Otworz kolejke systemu V "reprezentowana" przez KEY
+    **********************************/
+    key_t key = ftok(KEY, 0);
+    int queueID = msgget(key, 0);
+    if (queueID < 0) {
+        perror("msgget");
+        exit(1);
     }
+    /**********************************
+    Odczytaj zapisane w kolejce wartosci i przypisz je do zmiennej val
+    obowiazuja funkcje systemu V
+    ************************************/
+    struct mess m;
+    int val;
+    if (msgrcv(queueID, &m, sizeof(long), -100, 0) < 0) {
+        perror("Cannot receive message");
+        exit(1);
+    }
+    val = m.value;
+    printf("%d square is: %d\n", val, val * val);
+    /*******************************
+    posprzataj
+    ********************************/
+    msgctl(queueID, IPC_RMID, NULL);
 
-
-	while(1)
- 	{
-	    /**********************************
-	    Odczytaj zapisane w kolejce wartosci i przypisz je do zmiennej val
-	    obowiazuja funkcje systemu V
-	    ************************************/
-
-            if(msgrcv(q, &val, sizeof(val), 0, 0) < 0) {
-                perror("Cannot receive message");
-                exit(1);
-            };
-        	 printf("%d square is: %d\n", val, val*val);
-
-	}
-
-	/*******************************
-	posprzataj
-	********************************/
-    msgctl(q, IPC_RMID, NULL);
-
-     return 0;
-   }
+    return 0;
+}
--- kol2/zad9/sender.c
+++ kol2/zad9/sender.c
@@ -3,40 +3,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 
+#define KEY "/home/ajris"
 
-#define KEY "./queuekey"
+struct mess{
+    long mtype;
+    long value;
+};
 
-int main(int argc, char* argv[])
-{
-
- if(argc !=2){
-   printf("Not a suitable number of program parameters\n");
-   return(1);
- }
-
- /******************************************************
- Utworz kolejke komunikatow systemu V "reprezentowana" przez KEY
- Wyslij do niej wartosc przekazana jako parametr wywolania programu
- Obowiazuja funkcje systemu V
- ******************************************************/
-  key_t k = ftok(KEY, 1);
-    int q = msgget(k, 0644);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Not a suitable number of program parameters\n");
+        return (1);
+    }
+    /******************************************************
+    Utworz kolejke komunikatow systemu V "reprezentowana" przez KEY
+    Wyslij do niej wartosc przekazana jako parametr wywolania programu
+    Obowiazuja funkcje systemu V
+    ******************************************************/
+    key_t key = ftok(KEY, 0);
     int val = atoi(argv[1]);
-
-    struct msgbuf *buf = calloc(1, sizeof(long) + sizeof(val));
-
-    buf->mtype = 1;
-
-    memcpy(&buf->mtext, &val, sizeof(val));
-
-    if(msgsnd(q, &buf, sizeof(val), 0) < 0){
+    int queueID = msgget(key, 0666 | IPC_CREAT);
+    struct mess buf;
+    buf.mtype = 1;
+    buf.value = val;
+    if (msgsnd(queueID, &buf, sizeof(long), 0) < 0) {
         perror("msgsnd");
     }
-
-
-  return 0;
+    return 0;
 }
-
-
-
