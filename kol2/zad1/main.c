816d995d1f28b6a98d6af3f0a895bce8fae699d4
Ajris <hubiflower@gmail.com>
2019-06-03 13:52:16 +0200

    finished zad1

--- kol2/zad1/main.c
+++ kol2/zad1/main.c
@@ -7,49 +7,58 @@
 #include <sys/types.h>
 #include <pthread.h>
 
-void* hello(void* arg){
-        
-        sleep(1);
-        while(1){
-                printf("Hello world from thread number %d\n", *(int*)arg);
-		/****************************************************
-			przerwij dzialanie watku jesli bylo takie zadanie
-		*****************************************************/                
-                printf("Hello again world from thread number %d\n", *(int*)arg);
-                sleep(2);
-        }
-        return NULL;
+void *hello(void *arg) {
+
+    sleep(1);
+    while (1) {
+        printf("Hello world from thread number %d\n", *(int *) arg);
+        pthread_testcancel();
+        /****************************************************
+            przerwij dzialanie watku jesli bylo takie zadanie
+        *****************************************************/
+        printf("Hello again world from thread number %d\n", *(int *) arg);
+        sleep(2);
+    }
+    return NULL;
 }
 
 
-int main(int argc, char** args){
+int main(int argc, char **args) {
+    if (argc != 3) {
+        printf("Not a suitable number of program parameters\n");
+        return (1);
+    }
+    int n = atoi(args[1]);
+    /**************************************************
+        Utworz n watkow realizujacych funkcje hello
+    **************************************************/
+    pthread_t* pthread = malloc(sizeof(pthread_t) * n);
+    for(int i = 0; i < n; i++){
+        pthread_create(&pthread[i], NULL, hello, &i);
+    }
 
-       if(argc !=3){
-	    printf("Not a suitable number of program parameters\n");
-    	return(1);
-   	}
+    int i = 0;
+    while (i++ < atoi(args[2])) {
+        printf("Hello from main %d\n", i);
+        sleep(2);
+    }
 
-        int n = atoi(args[1]);
+    i = 0;
 
-	/**************************************************
-	    Utworz n watkow realizujacych funkcje hello
-	**************************************************/
-        
+    /*******************************************
+        "Skasuj" wszystke uruchomione watki i poczekaj na ich zakonczenie
+    *******************************************/
+    for(int i = 0; i < n; i++){
+        pthread_cancel(pthread[i]);
+    }
 
-        i = 0;
-        while(i++ < atoi(args[2])) {
-                printf("Hello from main %d\n",i);
-                sleep(2);
-        }
-        
-        i = 0;
+    for(int i = 0; i < n; i++){
+        pthread_join(pthread[i], NULL);
+    }
 
-	/*******************************************
-	    "Skasuj" wszystke uruchomione watki i poczekaj na ich zakonczenie
-	*******************************************/
-        printf("DONE");
-        
-        
-        return 0;
+    printf("DONE");
+
+
+    return 0;
 }
 
