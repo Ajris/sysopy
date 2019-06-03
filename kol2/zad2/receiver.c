93c816f219b2d11902ee2e6ea745ec8716616095
Ajris <hubiflower@gmail.com>
2019-06-03 15:24:03 +0200

    XD

--- kol2/zad2/receiver.c
+++ kol2/zad2/receiver.c
@@ -10,17 +10,20 @@
 #define SHM_NAME "/kol_shm"
 #define MAX_SIZE 1024
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
     sleep(1);
-    int val =0;
+    int val = 0;
+    int desc = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0666);
+    ftruncate(desc, MAX_SIZE);
+    int* tmp = mmap(NULL, MAX_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, desc, 0);
+    val = *tmp;
     /*******************************************
     Utworz/otworz posixowy obszar pamieci wspolnej "reprezentowany" przez SHM_NAME
     odczytaj zapisana tam wartosc i przypisz ja do zmiennej val
     posprzataj
     *********************************************/
-
-	printf("%d square is: %d \n",val, val*val);
+    shm_unlink(SHM_NAME);
+    printf("%d square is: %d \n", val, val * val);
     return 0;
 }
--- kol2/zad2/sender.c
+++ kol2/zad2/sender.c
@@ -10,19 +10,20 @@
 #define SHM_NAME "/kol_shm"
 #define MAX_SIZE 1024
 
-int main(int argc, char **argv)
-{
-    
-   if(argc !=2){
-    printf("Not a suitable number of program parameters\n");
-    return(1);
-   }
+int main(int argc, char **argv) {
 
-   /***************************************
-   Utworz/otworz posixowy obszar pamieci wspolnej "reprezentowany" przez SHM_NAME
-   zapisz tam wartosc przekazana jako parametr wywolania programu
-   posprzataj
-   *****************************************/
- 
+    if (argc != 2) {
+        printf("Not a suitable number of program parameters\n");
+        return (1);
+    }
+    int desc = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0666);
+    ftruncate(desc, MAX_SIZE);
+    int* t = mmap(NULL, MAX_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, desc, 0);
+    *t = atoi(argv[1]);
+    /***************************************
+    Utworz/otworz posixowy obszar pamieci wspolnej "reprezentowany" przez SHM_NAME
+    zapisz tam wartosc przekazana jako parametr wywolania programu
+    posprzataj
+    *****************************************/
     return 0;
 }
