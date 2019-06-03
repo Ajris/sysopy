4ee3472ec4b1d6ffe3b7ec7a69036202fb2498da
Ajris <hubiflower@gmail.com>
2019-06-03 16:07:11 +0200

    finished zad5

--- kol2/zad5/main.c
+++ kol2/zad5/main.c
@@ -9,124 +9,73 @@
 #include <sys/wait.h>
 #include <semaphore.h>
 
-
 #define FILE_NAME "common.txt"
 #define SEM_NAME "my_kol_sem"
 
+int main(int argc, char **args) {
+    if (argc != 4) {
+        printf("Not a suitable number of program parameters\n");
+        return (1);
+    }
+    /************************************
+      Utworz semafor posixowy. Ustaw jego wartosc na 1
+     *************************************/
+    sem_t *sem_id = sem_open(SEM_NAME, O_CREAT, 0644, 1);
+    int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-void print_sem_value(sem_t *sem){
-	int sem_val;
-	sem_getvalue(sem,&sem_val);
-	printf("Current sem value: %d\n",sem_val);	
-}
+    int parentLoopCounter = atoi(args[1]);
+    int childLoopCounter = atoi(args[2]);
 
-int main(int argc, char** args){
+    char buf[50];
+    pid_t childPid;
+    int max_sleep_time = atoi(args[3]);
+    if ((childPid = fork())) {
+        int status = 0;
+        srand((unsigned) time(0));
 
-	if(argc !=4){
-		printf("Not a suitable number of program parameters\n");
-		return(1);
-	}
+        while (parentLoopCounter--) {
+            int s = rand() % max_sleep_time + 1;
+            sleep(s);
+            /******************************************
+              Sekcja krytyczna. Zabezpiecz dostep semaforem
+             ******************************************/
+            sem_wait(sem_id);
 
-
-	/************************************
-	  Utworz semafor posixowy. Ustaw jego wartosc na 1
-	 *************************************/
-
-	sem_t* sem_id = sem_open(SEM_NAME, O_CREAT, 0644, 1);
-
-	print_sem_value(sem_id);
-
-
-	int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC , 0644);
-
-	int parentLoopCounter = atoi(args[1]);
-	int childLoopCounter = atoi(args[2]);
-
-	char buf[20];
-	pid_t childPid;
-	int max_sleep_time = atoi(args[3]);
-
-	if(childPid=fork()){
-		int status =0;
-		srand((unsigned)time(0)); 
-
-		while(parentLoopCounter--){
-			int s = rand()%max_sleep_time+1;
-			sleep(s);    
-
-			print_sem_value(sem_id);
-
-			/******************************************
-			  Sekcja krytyczna. Zabezpiecz dostep semaforem
-			 ******************************************/	
-
-			sem_wait(sem_id);
-
-			print_sem_value(sem_id);
-
-
-			sprintf(buf, "Wpis rodzica. Petla %d. Spalem %d\n", parentLoopCounter,s);
-			write(fd, buf, strlen(buf));
-			write(1, buf, strlen(buf));
-
-			print_sem_value(sem_id);
-
-			/****************************************************
-			  Koniec sekcji krytycznej
-			 ****************************************************/
-
-			sem_post(sem_id);
-
-			print_sem_value(sem_id);
-
-
-		}
-		waitpid(childPid,&status,0);
-	}
-	else{
-
-		srand((unsigned)time(0)); 
-		while(childLoopCounter--){
-
-			int s = rand()%max_sleep_time+1;
-			sleep(s);                
-
-			print_sem_value(sem_id);
-
-			/******************************************
-			  Sekcja krytyczna. Zabezpiecz dostep semaforem
-			 ******************************************/
-
-			sem_wait(sem_id);
-
-			print_sem_value(sem_id);
-
-			sprintf(buf, "Wpis dziecka. Petla %d. Spalem %d\n", childLoopCounter,s);
-			write(fd, buf, strlen(buf));
-			write(1, buf, strlen(buf));
-
-			print_sem_value(sem_id);
-
-			/****************************************************
-			  Koniec sekcji krytycznej
-			 ****************************************************/
-
-			sem_post(sem_id);
-
-			print_sem_value(sem_id);
-
-		}
-		_exit(0);
-	}
-
-	/***************************************
-	  Posprzataj semafor
-	 ***************************************/
-
-	sem_close(sem_id);
-	sem_unlink(SEM_NAME);
-	close(fd);
-	return 0;
+            sprintf(buf, "Wpis rodzica. Petla %d. Spalem %d\n", parentLoopCounter, s);
+            write(fd, buf, strlen(buf));
+            write(1, buf, strlen(buf));
+            /****************************************************
+              Koniec sekcji krytycznej
+             ****************************************************/
+            sem_post(sem_id);
+        }
+        waitpid(childPid, &status, 0);
+    } else {
+        srand((unsigned) time(0));
+        while (childLoopCounter--) {
+            int s = rand() % max_sleep_time + 1;
+            sleep(s);
+            /******************************************
+              Sekcja krytyczna. Zabezpiecz dostep semaforem
+             ******************************************/
+            sem_wait(sem_id);
+            sprintf(buf, "Wpis dziecka. Petla %d. Spalem %d\n", childLoopCounter, s);
+            write(fd, buf, strlen(buf));
+            write(1, buf, strlen(buf));
+            /****************************************************
+              Koniec sekcji krytycznej
+             ****************************************************/
+            sem_post(sem_id);
+        }
+        _exit(0);
+    }
+    /***************************************
+      Posprzataj semafor
+     ***************************************/
+    sem_close(sem_id);
+    sem_unlink(SEM_NAME);
+    close(fd);
+    return 0;
 }
 
 
