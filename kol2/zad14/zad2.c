e955823960aa22acf6ec287de05e994d18601e3c
Ajris <hubiflower@gmail.com>
2019-06-03 23:29:52 +0200

    zad14?

--- kol2/zad14/zad2.c
+++ kol2/zad14/zad2.c
@@ -13,103 +13,79 @@
 
 
 union semun {                   /* Uzywane do semctl() */
-    int                 val;
-    struct semid_ds *   buf;
-    unsigned short *    array;
-    struct seminfo *    __buf;
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+    struct seminfo *__buf;
 };
 
-typedef enum { FALSE, TRUE } Boolean;
-
+typedef enum {
+    FALSE, TRUE
+} Boolean;
 
 Boolean bsUseSemUndo = FALSE;
 Boolean bsRetryOnEintr = TRUE;
 
-
-int                     /* Inicjalizacja dostepnego semafora*/
-initSemAvailable(int semId, int semNum)
-{
+int initSemAvailable(int semId, int semNum) {
     union semun arg;
-
     arg.val = 1;
     return semctl(semId, semNum, SETVAL, arg);
 }
 
-int                     /* Inicjalizacja zajetego semafora */
-initSemInUse(int semId, int semNum)
-{
+int initSemInUse(int semId, int semNum) {
     union semun arg;
-
     arg.val = 0;
     return semctl(semId, semNum, SETVAL, arg);
 }
 
-/* Operacja P*/
-
-int                     
-reserveSem(int semId, int semNum)
-{
+int reserveSem(int semId, int semNum) {
     struct sembuf sops;
-
+    sops.sem_num = semNum;
+    sops.sem_op = -1;
+    sops.sem_flg = 0;
+    semop(semId, &sops, 1);
     /* Ustaw odpowiednie pola struktury sops i wykonaj 
        operacje na semaforze semid*/
 
     return 0;
 }
 
-int     /* Operacja V */
-releaseSem(int semId, int semNum)
-{
+int releaseSem(int semId, int semNum) {
     struct sembuf sops;
-     /* Ustaw odpowiednie pola struktury sops i wykonaj 
-       operacje na semaforze semid*/
-    sops -> sem_num = semNum;
-    sops -> sem_op = 1;
-    sops -> sem_flg = 0;
-    return semop(semId, sops);
-
+    /* Ustaw odpowiednie pola struktury sops i wykonaj
+      operacje na semaforze semid*/
+    sops.sem_num = semNum;
+    sops.sem_op = 1;
+    sops.sem_flg = 0;
+    return semop(semId, &sops, 1);
 }
 
 
-int
-main(int argc, char *argv[])
-{
-    int semId=1;
-     int s;
+int main(int argc, char *argv[]) {
+    int semId = 1;
+    int s;
 
-     
-    if (argc < 2 || argc > 3 || strcmp(argv[1], "--help") == 0)
-    {
-        printf("%s good OR %s bad\n",argv[0],argv[0]);
+    if (argc < 2 || argc > 3 || strcmp(argv[1], "--help") == 0) {
+        printf("%s good OR %s bad\n", argv[0], argv[0]);
         exit(-1);
     }
 
+    semId = semget(IPC_PRIVATE, 1, S_IRUSR | S_IWUSR);
 
-     semId = semget(IPC_PRIVATE, 1, S_IRUSR | S_IWUSR);
-
-    if(strcmp(argv[1], "good") == 0)
-    {
-     s = initSemAvailable(semId, 0);
-
-     s = reserveSem(semId, 0);
-     printf("Sekcja krytyczna! \n");
-     s = releaseSem(semId, 0);
-
-    }
-    else if(strcmp(argv[1], "bad") == 0) 
-    {
-
-     s = initSemInUse(semId, 0);
-
-     s = reserveSem(semId, 0);
-     printf("Sekcja krytyczna! \n");
-     s = releaseSem(semId, 0);
-    }
-    else 
-     {
+    if (strcmp(argv[1], "good") == 0) {
+        s = initSemAvailable(semId, 0);
+        s = reserveSem(semId, 0);
+        printf("Sekcja krytyczna! \n");
+        s = releaseSem(semId, 0);
+    } else if (strcmp(argv[1], "bad") == 0) {
+        s = initSemInUse(semId, 0);
+        s = reserveSem(semId, 0);
+        printf("Sekcja krytyczna! \n");
+        s = releaseSem(semId, 0);
+    } else {
         printf("Argument error \n");
-        exit(-1); 
-     }
+        exit(-1);
+    }
 
     exit(EXIT_SUCCESS);
 }
