fdb5636d6b476f66e193647653362f8c5643a183
Ajris <hubiflower@gmail.com>
2019-06-03 17:01:58 +0200

    finished zad8

--- kol2/zad8/receiver.c
+++ kol2/zad8/receiver.c
@@ -6,41 +6,33 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-#define UNIX_PATH_MAX 108
 #define SOCK_PATH "sock_path"
-
 
 int main() {
     int fd = -1;
-
     /*********************************************
     Utworz socket domeny unixowej typu datagramowego
     Utworz strukture adresowa ustawiajac adres/sciezke komunikacji na SOCK_PATH
     Zbinduj socket z adresem/sciezka SOCK_PATH
     **********************************************/
     struct sockaddr_un addr = {
-        .sun_family = AF_UNIX,
-        .sun_path = SOCK_PATH
+            .sun_family = AF_UNIX,
+            .sun_path = SOCK_PATH
     };
-
     fd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    bind(fd,(const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
-
+    bind(fd, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
 
     char buf[20];
-    if(read(fd, buf, 20) == -1)
+    if (read(fd, buf, 20) == -1)
         perror("Error receiving message");
     int val = atoi(buf);
-    printf("%d square is: %d\n",val,val*val);
-
-
+    printf("%d square is: %d\n", val, val * val);
     /***************************
     Posprzataj po sockecie
     ****************************/
     shutdown(fd, SHUT_RDWR);
     close(fd);
     unlink(SOCK_PATH);
-
     return 0;
 }
 
